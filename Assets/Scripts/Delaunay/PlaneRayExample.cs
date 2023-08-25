295393d32e3eba5e188681f8fb932f80cfc1dc46
AndyP <malicioussoap@gmail.com>
2023-08-25 17:03:42 -0500

    scene changes

--- Assets/Scripts/Delaunay/PlaneRayExample.cs
+++ Assets/Scripts/Delaunay/PlaneRayExample.cs
@@ -19,18 +19,13 @@ public class PlaneRayExample : MonoBehaviour
 
     void Start()
     {
-        //This is how far away from the Camera the plane is placed
-        //m_DistanceFromCamera = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z - m_DistanceZ);
-
-        //Create a new plane with normal (0,0,1) at the position away from the camera you define in the Inspector. This is the plane that you can click so make sure it is reachable.
-       // m_Plane = new Plane(Vector3.forward, m_DistanceFromCamera);
+        
     }
 
     void Update()
     {
         
-        //Detect when there is a mouse click
-        if (true)//Input.GetMouseButton(0))
+        if (true)
         {
             //Create a ray from the Mouse click position
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -38,12 +33,7 @@ public class PlaneRayExample : MonoBehaviour
 
             if (m_Plane.GetComponent<MeshCollider>().Raycast(ray, out RaycastHit hitPoint, 10000))
             {
-                
- 
 
-                //Move your cube GameObject to the point where you clicked
-                
-                
                 m_Cube.transform.position = new Vector3(
                     Mathf.RoundToInt(hitPoint.point.x), 
                     0, 
@@ -51,7 +41,7 @@ public class PlaneRayExample : MonoBehaviour
                     );
 
                 lastHit = hitPoint;
-                //m_Cube.transform.position = hitPoint.point; 
+                
             }
         }
 
